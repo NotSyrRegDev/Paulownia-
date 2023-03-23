fa5f0bd0f282e80e94320bf50958ff633d4e18da
NotRegSyrDev <55794684+NotSyrRegDev@users.noreply.github.com>
2023-03-23 23:14:02 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
   return (
     <div className='section fotter_section' >
         <footer>
-                      <div className="footer_area">
+        <div className="footer_area">
             <h1 className="footer_subheadline">بني ❤️ بواسطة  
-              <span onClick={() => openInNewTab('https://notregsyrdev.web.app/') } > عمار حمدي حموش  - </span>
+              <span onClick={() => openInNewTab('https://notregsyrdev.web.app/') } > عمار حمدي حموش  </span>
             
                </h1>
             </div>
